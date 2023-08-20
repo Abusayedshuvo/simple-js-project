0e13b0cae439cd270ba9f71f260d840aa1efc292
Abusayed Shuvo <abusayedshuvo24@gmail.com>
2023-08-20 11:07:39 +0600

    done all function

--- js/script.js
+++ js/script.js
@@ -13,24 +13,32 @@ function cart(target) {
   p.classList.add("text-2xl", "font-medium", "my-3");
   p.innerText = `${count + 1}. ${productName}`;
   productNameArea.appendChild(p);
-}
 
-// coupon function
-document.getElementById("coupon").addEventListener("keyup", function () {
-  const couponCode = document.getElementById("coupon").value;
+  // Purchase Button Function
+  const purchaseBtn = document.getElementById("purchase-btn");
+  if (totalPrice > 0) {
+    purchaseBtn.removeAttribute("disabled");
+  } else {
+    purchaseBtn.setAttribute("disabled", "true");
+  }
+
+  // coupon function
   const couponBtn = document.getElementById("coupon-btn");
-  if (couponCode === "SELL200") {
+  if (totalPrice > 200) {
     couponBtn.removeAttribute("disabled");
   } else {
     couponBtn.setAttribute("disabled", "true");
   }
-});
-
-// Purchase Button Function
-const purchaseBtn = document.getElementById("purchase-btn");
-const newTotalPrice = document.getElementById("total-price").innerText;
-if (totalPrice >= "200") {
-  purchaseBtn.removeAttribute("disabled");
 }
 
-console.log(newTotalPrice);
+// coupon function
+function couponCode() {
+  const couponInput = document.getElementById("coupon").value;
+  const discountElement = document.getElementById("discount");
+  const total = document.getElementById("total");
+  if (couponInput === "SELL200") {
+    const discount = totalPrice * 0.2;
+    discountElement.innerText = discount.toFixed(2);
+    total.innerText = totalPrice - discount;
+  }
+}
